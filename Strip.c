28fe0ab9364b401f1bd3f00596becd41ecdc9ba8
Mateusz Świetlicki <mateusz@swietlicki.net>
2013-11-15 15:16:18 +0100

    Optimize i + index

--- Strip.c
+++ Strip.c
@@ -154,22 +154,23 @@ int main(void){
 
 		for (i = 0; i < numLEDs; i++)
 		{
+			uint8_t ii = (i + index) % numLEDs;
 			int h = hn * i;
 			int a = (h % 120) * 4;
 			uint8_t x = 255 - abs(a - 255);
 
 			if (h < 60)
-				SetPixel(i + index, 255, x, 0);
+				SetPixel(ii, 255, x, 0);
 			else if (h < 120)
-				SetPixel(i + index, x, 255, 0);
+				SetPixel(ii, x, 255, 0);
 			else if (h < 180)
-				SetPixel(i + index, 0, 255, x);
+				SetPixel(ii, 0, 255, x);
 			else if (h < 240)
-				SetPixel(i + index, 0, x, 255);
+				SetPixel(ii, 0, x, 255);
 			else if (h < 320)
-				SetPixel(i + index, x, 0, 255);
+				SetPixel(ii, x, 0, 255);
 			else
-				SetPixel(i + index, 255, 0, x);
+				SetPixel(ii, 255, 0, x);
 		}
 
 		Show();
